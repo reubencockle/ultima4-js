2140db20a7153f8267c1a97be3f1baec255dd88c
Reuben Cockle <reuben@AKLW3006835.local>
2019-01-02 14:28:07 +1300

    Enter/leave towns Added maps for britain and paws

---
+++ tools/u4tojs/u4tojs/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace u4tojs
+{
+	class Program
+	{
+		static void Usage()
+		{
+			Console.WriteLine(@"
+u4tojs - convert Ultima 4 maps to javascript
+
+USAGE: u4tojs <data-file> <map-file> <js-file>
+");
+		}
+
+		static void Main(string[] args)
+		{
+			if (args.Length != 3) { Usage(); return; }
+
+			try
+			{
+				var world = args[0];
+				var datafile = args[1];
+				var jsmap = args[2];
+
+				Console.WriteLine($"Loading {world}...");
+				var map = File.ReadAllBytes(world);
+				StreamWriter s = new StreamWriter(jsmap);
+				var name = Path.GetFileNameWithoutExtension(datafile).ToLowerInvariant();
+				s.WriteLine($"maps[\"{name}\"] = {{");
+
+				int mapWidth = 0;
+				int mapHeight = 0;
+				var dat = new StreamReader(datafile);
+				string line;
+				while ((line = dat.ReadLine()) != null)
+				{
+					s.WriteLine(line);
+
+					var match = Regex.Match(line, @"^(\w\w*)\s*:\s*(\d\d*)");
+					if (match.Success)
+					{
+						switch (match.Groups[1].Value)
+						{
+							case "width":
+								if (mapWidth == 0) mapWidth = Convert.ToInt32(match.Groups[2].Value);
+								break;
+
+							case "height":
+								if (mapHeight == 0) mapHeight = Convert.ToInt32(match.Groups[2].Value);
+								break;
+						}
+					}
+				}
+				dat.Close();
+
+				if (mapWidth == 0 || mapHeight == 0) throw new ArgumentException($"Map width or height not defined in {datafile}");
+				s.WriteLine("data:\"\\");
+
+				for (int y = 0; y < mapHeight; y++)
+				{
+					for (int x = 0; x < mapWidth; x++)
+					{
+						var b = getTile(map, x, y);
+						s.Write(b.ToString("X2"));
+					}
+					s.WriteLine("\\");
+				}
+				s.WriteLine("\"};");
+				s.Flush();
+				s.Close();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"ERROR: {ex.Message}");
+				Console.WriteLine("run without arguments for usage");
+			}
+		}
+
+		/// <summary>
+		/// Gets a tile from the map converting from absolute coords to (32 x 32 byte) blocks
+		/// </summary>
+		static byte getTile(byte[] map, int x, int y)
+		{
+			int bx = x >> 5;
+			int by = y >> 5;
+			int dx = x & 31;
+			int dy = y & 31;
+			int a = (by * 8 + bx) * 32 * 32 + dy * 32 + dx;
+			return map[a];
+		}
+	}
+}
---
+++ tools/u4tojs/vismap/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+
+namespace vismap
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int width = 11;
+			int height = 11;
+			int offsetX = width / 2;
+			int offsetY = height / 2;
+			string s =
+			"..........." +
+			"..........." +
+			"..........." +
+			"..........." +
+			".....#....." +
+			"..#..X....." +
+			".........#." +
+			".........#." +
+			"...##....#." +
+			"..........." +
+			"...........";
+			var map = new Map<char>(width, height, s.ToCharArray());
+			var vis = new Map<int>(width, height);
+
+			for (int size = 1; size <= 5; size++)
+			{
+				dovis(size*2, -size, -size, 1, 0, vis, map);
+				dovis(size * 2, size, -size, 0, 1, vis, map);
+				dovis(size * 2, size, size, -1, 0, vis, map);
+				dovis(size * 2, -size, size, 0, -1, vis, map);
+
+				//// top
+				//for (int x = -size; x < size; x++)
+				//{
+				//	int c = map.get(x, -size) == '#' ? 1 : 0;
+				//	int v = vis.get(x - sign(x), -size + 1);
+				//	vis.set(x, -size, c+v);
+				//}
+
+				//// rigth
+				//for (int y = -size; y < size; y++)
+				//{
+				//	int c = map.get(size, y) == '#' ? 1 : 0;
+				//	int v = vis.get(size-1, y-sign(y));
+				//	vis.set(size, y, c+v);
+				//}
+
+				//// bottom
+				//for (int x = size; x > -size; x--)
+				//{
+				//	int c = map.get(x, size) == '#' ? 1 : 0;
+				//	int v = vis.get(x-sign(x), size-1);
+				//	vis.set(x, size, c+v);
+				//}
+
+				//// left
+				//for (int y = size; y > -size; y--)
+				//{
+				//	int c = map.get(-size, y) == '#' ? 1 : 0;
+				//	int v = vis.get(-size + 1, y - sign(y));
+				//	vis.set(-size, y, c+v);
+				//}
+			}
+		}
+
+		static void dovis(int n, int x, int y, int dx, int dy, Map<int> vis, Map<char> map)
+		{
+			for (int i = 0; i < n; i++)
+			{
+				int c = map.get(x, y) == '#' ? 1 : 0;
+				int v = vis.get(x - sign(x), y - sign(y));
+				vis.set(x, y, c + v);
+				x += dx;
+				y += dy;
+			}
+		}
+
+		static int slope(int x, int y)
+		{
+			double d = (double)x / (double)y;
+			return (int)Math.Round(d);
+		}
+
+		int getVis(int[] vis, int x, int y, int w, int h)
+		{
+			return vis[y * w + x];
+		}
+		static int sign(int i)
+		{
+			return i < 0 ? -1 : i > 0 ? 1 : 0;
+		}
+	}
+
+	class Map<T> {
+		T[] map;
+		int width;
+		int height;
+		int offsetX;
+		int offsetY;
+
+		public Map(int width, int height, T[] map = null)
+		{
+			this.width = width;
+			this.height = height;
+			this.offsetX = width / 2;
+			this.offsetY = height / 2;
+			this.map = map!=null ? map : new T[width * height];
+		}
+
+		public T get(int x, int y)
+		{
+			return map[(y+offsetY) * width + (x+offsetX)];
+		}
+
+		public void set(int x, int y, T c)
+		{
+			map[(y + offsetY) * width + (x + offsetX)] = c;
+			draw();
+
+		}
+
+		public void draw()
+		{
+			Console.Clear();
+			for (int y = 0; y < height; y++)
+			{
+				for (int x = 0; x < width; x++)
+				{
+					Console.Write(get(x-offsetX, y-offsetY));
+				}
+				Console.WriteLine();
+			}
+			System.Threading.Thread.Sleep(100);
+		}
+	}
+}
